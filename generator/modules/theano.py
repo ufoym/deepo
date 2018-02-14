c0656838da06d33a49e84d8fa059b387deb715d5
Ming <a@ufoym.com>
2018-02-14 14:14:39 +0800

    Fix theano

--- generator/modules/theano.py
+++ generator/modules/theano.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
-from .__module__ import Module, dependency, source
+from .__module__ import Module, dependency, source, version
 from .tools import Tools
 from .python import Python
 
 
 @dependency(Tools, Python)
 @source('git')
+@version('1.0.1')
 class Theano(Module):
 
     def build(self):
         return r'''
-            $GIT_CLONE https://github.com/Theano/Theano ~/theano && \
-            cd ~/theano && \
+            DEBIAN_FRONTEND=noninteractive $APT_INSTALL \
+                libblas-dev \
+                && \
+
+            wget -qO- https://github.com/Theano/Theano/archive/rel-%s.tar.gz''' \
+            % self.version + r''' | tar xz -C ~ && \
+            cd ~/Theano* && \
             $PIP_INSTALL \
                 . && \
         ''' + (
@@ -26,7 +32,7 @@ class Theano(Module):
             python setup.py build && \
             python setup.py install && \
 
-            printf '[global]\nfloatX = float32\ndevice = cuda0\n\n[dnn]\n''' \
-            + r'''include_path = /usr/local/cuda/targets''' \
+            printf '[global]\nfloatX = float32\ndevice = cuda0\n\n[dnn]\n'''
+            + r'''include_path = /usr/local/cuda/targets'''
             + r'''/x86_64-linux/include\n' > ~/.theanorc && \
         ''')
