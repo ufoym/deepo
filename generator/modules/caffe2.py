ad26f6d85f10e110eb67b5a748c7e001f706a587
Ming <a@ufoym.com>
2018-02-25 15:35:32 +0800

    Add Caffe2 (#23)
    Resolve #7

---
+++ generator/modules/caffe2.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from .__module__ import Module, dependency, source
+from .tools import Tools
+from .python import Python
+from .opencv import Opencv
+
+
+@dependency(Tools, Python, Opencv)
+@source('git')
+class Caffe2(Module):
+
+    def build(self):
+        pyver = self.composer.ver(Python)
+        return r'''
+            DEBIAN_FRONTEND=noninteractive $APT_INSTALL \
+                libprotobuf-dev \
+                protobuf-compiler \
+                && \
+
+            $PIP_INSTALL \
+                future \
+                numpy \
+                protobuf \
+                && \
+
+            $GIT_CLONE https://github.com/caffe2/caffe2.git \
+                ~/caffe2 --recursive && \
+
+            cd ~/caffe2 && \
+            sed -i "s/prefix=''/prefix='', standard_lib=True) \
+                + '\/dist-packages'/g" CMakeLists.txt && \
+
+            mkdir build && cd build && \
+            cmake -D CMAKE_BUILD_TYPE=RELEASE \
+                  -D CMAKE_INSTALL_PREFIX=/usr/local \
+                  -D USE_CUDA=%s \
+                  .. && \
+            make -j"$(nproc)" install && \
+            %s
+        ''' % (
+            'OFF' if self.composer.cpu_only else 'ON',
+
+            '' if pyver == '2.7' else (r'''
+            mv /usr/local/lib/python3/dist-packages/caffe2 \
+                /usr/local/lib/python%s/dist-packages && \
+            ''' % pyver).strip()
+            )
--- scripts/gen-docker.sh
+++ scripts/gen-docker.sh
@@ -18,11 +18,13 @@ python ../generator/generate.py ../docker/Dockerfile.lasagne-py27 lasagne python
 python ../generator/generate.py ../docker/Dockerfile.lasagne-py36 lasagne python==3.6
 python ../generator/generate.py ../docker/Dockerfile.caffe-py27 caffe python==2.7
 python ../generator/generate.py ../docker/Dockerfile.caffe-py36 caffe python==3.6
+python ../generator/generate.py ../docker/Dockerfile.caffe2-py27 caffe2 python==2.7
+python ../generator/generate.py ../docker/Dockerfile.caffe2-py36 caffe2 python==3.6
 python ../generator/generate.py ../docker/Dockerfile.torch torch
-python ../generator/generate.py ../docker/Dockerfile.all-py27 tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe torch python==2.7
-python ../generator/generate.py ../docker/Dockerfile.all-py36 tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe torch python==3.6
-python ../generator/generate.py ../docker/Dockerfile.all-py27-jupyter tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe torch python==2.7 jupyter
-python ../generator/generate.py ../docker/Dockerfile.all-py36-jupyter tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe torch python==3.6 jupyter
+python ../generator/generate.py ../docker/Dockerfile.all-py27 tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe caffe2 torch python==2.7
+python ../generator/generate.py ../docker/Dockerfile.all-py36 tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe caffe2 torch python==3.6
+python ../generator/generate.py ../docker/Dockerfile.all-py27-jupyter tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe caffe2 torch python==2.7 jupyter
+python ../generator/generate.py ../docker/Dockerfile.all-py36-jupyter tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe caffe2 torch python==3.6 jupyter
 python ../generator/generate.py ../docker/Dockerfile.tensorflow-py27-cpu tensorflow python==2.7 --cpu-only
 python ../generator/generate.py ../docker/Dockerfile.tensorflow-py36-cpu tensorflow python==3.6 --cpu-only
 python ../generator/generate.py ../docker/Dockerfile.sonnet-py27-cpu sonnet python==2.7 --cpu-only
@@ -41,8 +43,10 @@ python ../generator/generate.py ../docker/Dockerfile.lasagne-py27-cpu lasagne py
 python ../generator/generate.py ../docker/Dockerfile.lasagne-py36-cpu lasagne python==3.6 --cpu-only
 python ../generator/generate.py ../docker/Dockerfile.caffe-py27-cpu caffe python==2.7 --cpu-only
 python ../generator/generate.py ../docker/Dockerfile.caffe-py36-cpu caffe python==3.6 --cpu-only
+python ../generator/generate.py ../docker/Dockerfile.caffe2-py27-cpu caffe2 python==2.7 --cpu-only
+python ../generator/generate.py ../docker/Dockerfile.caffe2-py36-cpu caffe2 python==3.6 --cpu-only
 python ../generator/generate.py ../docker/Dockerfile.torch-cpu torch --cpu-only
-python ../generator/generate.py ../docker/Dockerfile.all-py27-cpu tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe torch python==2.7 --cpu-only
-python ../generator/generate.py ../docker/Dockerfile.all-py36-cpu tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe torch python==3.6 --cpu-only
-python ../generator/generate.py ../docker/Dockerfile.all-py27-jupyter-cpu tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe torch python==2.7 jupyter --cpu-only
-python ../generator/generate.py ../docker/Dockerfile.all-py36-jupyter-cpu tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe torch python==3.6 jupyter --cpu-only
+python ../generator/generate.py ../docker/Dockerfile.all-py27-cpu tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe caffe2 torch python==2.7 --cpu-only
+python ../generator/generate.py ../docker/Dockerfile.all-py36-cpu tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe caffe2 torch python==3.6 --cpu-only
+python ../generator/generate.py ../docker/Dockerfile.all-py27-jupyter-cpu tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe caffe2 torch python==2.7 jupyter --cpu-only
+python ../generator/generate.py ../docker/Dockerfile.all-py36-jupyter-cpu tensorflow sonnet mxnet cntk keras pytorch chainer theano lasagne caffe caffe2 torch python==3.6 jupyter --cpu-only
