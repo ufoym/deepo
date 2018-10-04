0c302ed755166483e78670608c675cd65f8321b9
Ming <a@ufoym.com>
2018-10-04 22:46:41 +0800

    Keep PyTorch updated (use nightly build instead)

--- generator/modules/pytorch.py
+++ generator/modules/pytorch.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
-from .__module__ import Module, dependency, source, version
+from .__module__ import Module, dependency, source
 from .python import Python
 
 
 @dependency(Python)
 @source('pip')
-@version('0.4.1')
 class Pytorch(Module):
 
     def build(self):
-        pyver = self.composer.ver(Python)
         cuver = 'cpu' if self.composer.cuda_ver is None else 'cu%d' % (
             float(self.composer.cuda_ver) * 10)
-        platform = 'cp35-cp35m' if pyver == '3.5' else (
-            'cp36-cp36m' if pyver == '3.6' else 'cp27-cp27mu')
         return r'''
             $PIP_INSTALL \
-                http://download.pytorch.org/whl/%s/''' % cuver \
-        + r'''torch-%s-%s-linux_x86_64.whl \
-                torchvision \
+                torch_nightly -f \
+                https://download.pytorch.org/whl/nightly/%s/torch_nightly.html \
                 && \
-        ''' % (self.version, platform)
+        ''' % cuver
