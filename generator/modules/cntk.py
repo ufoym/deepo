8c5d4544bd5688e798e2482f40ccfcfe57b2a64e
Ming <a@ufoym.com>
2018-10-11 16:09:40 +0800

    Fix CNTK

--- generator/modules/cntk.py
+++ generator/modules/cntk.py
@@ -20,10 +20,9 @@ class Cntk(Module):
                 libjasper-dev \
                 && \
 
-            wget -O ~/mklml.tgz \
-                https://github.com/01org/mkl-dnn/releases/download/v0.14/mklml_lnx_2018.0.3.20180406.tgz && \
-            mkdir /usr/local/mklml && \
-            tar -xzf ~/mklml.tgz -C /usr/local/mklml && \
+            wget --no-verbose -O - https://github.com/01org/mkl-dnn/releases/download/v0.14/mklml_lnx_2018.0.3.20180406.tgz | tar -xzf - && \
+            cp mklml*/* /usr/local -r && \
+
             wget --no-verbose -O - https://github.com/01org/mkl-dnn/archive/v0.14.tar.gz | tar -xzf - && \
             cd mkl-dnn-0.14 && mkdir build && cd build && \
             ln -s /usr/local external && \
