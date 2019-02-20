ed13a79cbf94826c3938483846d96ca304d95806
Ming <a@ufoym.com>
2019-02-20 08:38:57 +0800

    Fix libjasper issue

--- generator/modules/cntk.py
+++ generator/modules/cntk.py
@@ -10,14 +10,14 @@ class Cntk(Module):
 
     def build(self):
         pyver = self.composer.ver(Python)
-        platform = 'cp35-cp35m' if pyver == '3.5' else (
-            'cp36-cp36m' if pyver == '3.6' else 'cp27-cp27mu')
+        platform = 'cp27-cp27mu' if pyver == '2.7' else (
+            'cp35-cp35m' if pyver == '3.5' else 'cp36-cp36m')
         return r'''
             DEBIAN_FRONTEND=noninteractive $APT_INSTALL \
                 openmpi-bin \
                 libpng-dev \
+                libjpeg-dev \
                 libtiff-dev \
-                libjasper-dev \
                 && \
 
             wget --no-verbose -O - https://github.com/01org/mkl-dnn/releases/download/v0.14/mklml_lnx_2018.0.3.20180406.tgz | tar -xzf - && \
