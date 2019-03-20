62190454d083b208c265e29cfe685c4fab90f8ce
Ming <a@ufoym.com>
2019-03-20 15:20:31 +0800

    Update tf

--- generator/modules/tensorflow.py
+++ generator/modules/tensorflow.py
@@ -8,16 +8,12 @@ from .python import Python
 class Tensorflow(Module):
 
     def build(self):
-        tf_version = self.composer.ver(Tensorflow)
-        if self.composer.cuda_ver == '8.0':
-            tf_version = '1.4'
-        tf_version = '' if 'latest' == tf_version else '==' + tf_version
         is_gpu = '' if self.composer.cuda_ver is None else '-gpu'
         return r'''
             $PIP_INSTALL \
-                tensorflow%s%s \
+                tf-nightly%s-2.0-preview
                 && \
-        ''' % (is_gpu, tf_version)
+        ''' % is_gpu
 
     def expose(self):
         return [
