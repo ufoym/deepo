59f7b3c7ed3bfb7cda7a38f771604e25ddf08fee
aisensiy <aisensiy@163.com>
2019-04-10 15:43:21 +0800

    Support tensorflow versions

--- generator/modules/tensorflow.py
+++ generator/modules/tensorflow.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
-from .__module__ import Module, dependency, source
+from .__module__ import Module, dependency, source, version
 from .python import Python
 
 
 @dependency(Python)
+@version('latest')
 @source('pip')
 class Tensorflow(Module):
 
+    def __init__(self, manager, **args):
+        super(self.__class__, self).__init__(manager, **args)
+        if self.version not in ('1.13.1', '2.0.0a0', 'latest'):
+            raise NotImplementedError('unsupported tensorflow version')
+
     def build(self):
         is_gpu = '' if self.composer.cuda_ver is None else '-gpu'
+        tensorflow_version = ("tf-nightly%s-2.0-preview" % is_gpu) if self.version == 'latest' else ("tensorflow%s-%s" % (is_gpu, self.version))
         return r'''
             $PIP_INSTALL \
-                tf-nightly%s-2.0-preview \
+                %s \
                 && \
-        ''' % is_gpu
+        ''' % tensorflow_version
 
     def expose(self):
         return [
