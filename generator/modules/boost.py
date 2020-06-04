b9c496d6447d6cc8f1b1981cbb024e139baa06a7
Ming <a@ufoym.com>
2020-06-04 15:04:55 +0800

    Simplify boost installation

--- generator/modules/boost.py
+++ generator/modules/boost.py
@@ -11,20 +11,27 @@ class Boost(Module):
     def __repr__(self):
         return ''
 
+    # def build(self):
+    #     pyver = self.composer.ver(Python)
+    #     return r'''
+    #         DEBIAN_FRONTEND=noninteractive $APT_INSTALL \
+    #             libboost-all-dev \
+    #             && \
+    #         ''' if pyver == '2.7' else (
+    #         r'''
+    #         wget -O ~/boost.tar.gz '''
+    #         + r'''https://dl.bintray.com/boostorg/release/1.69.0'''
+    #         + r'''/source/boost_1_69_0.tar.gz && \
+    #         tar -zxf ~/boost.tar.gz -C ~ && \
+    #         cd ~/boost_* && \
+    #         ./bootstrap.sh --with-python=python%s && \
+    #         ./b2 install -j"$(nproc)" --prefix=/usr/local && \
+    #         ''' % pyver
+    #     )
+
     def build(self):
-        pyver = self.composer.ver(Python)
         return r'''
             DEBIAN_FRONTEND=noninteractive $APT_INSTALL \
                 libboost-all-dev \
                 && \
-            ''' if pyver == '2.7' else (
-            r'''
-            wget -O ~/boost.tar.gz '''
-            + r'''https://dl.bintray.com/boostorg/release/1.69.0'''
-            + r'''/source/boost_1_69_0.tar.gz && \
-            tar -zxf ~/boost.tar.gz -C ~ && \
-            cd ~/boost_* && \
-            ./bootstrap.sh --with-python=python%s && \
-            ./b2 install -j"$(nproc)" --prefix=/usr/local && \
-            ''' % pyver
-        )
+        '''
